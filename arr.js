239c850f51bfb25d0bbb51918116742e3587ae2e
hxy <1061811549@qq.com>
2020-05-08 23:10:55 +0800

    解决operRepeatArray的bug 新增distinct方法

--- arr.js
+++ arr.js
@@ -1,99 +1,103 @@
 /**
- * 获取数组重复/未重复/去重复的元素
+ * 操作重复数组,获取数组重复/未重复/去重复的元素(会破坏已有排序)
  * @param {Array} array 源数组 
- * @param {Number} type 0返回重复数组 1返回未重复数组 2返回去重复数组
- * @param {Objecy} object 针对数组中对象重复判断 
- * {
- * 
- *    key: "num", //字段key
- * 
- *    flag: false //默认false 返回对象 true 返回对应key的value 
- * 
- * }
+ * @param {Number} type 默认为0 0返回去重复数组 1返回重复数组 2返回未重复数组
+ * @param {Objecy} object 针对数组中对象重复判断
+ - {
+ -   key: "num", //字段key
+ -   flag: false //默认false true返回对象  false返回对应key的value 
+ - }
  * @returns {Array} 返回数组
  */
-function refrain(array, type, object) {
-  let arr = [],//重复
-    arr1 = [],//未重复
+function operRepeatArray(array, type, object) {
+  let repeat = [],//重复
+    noRepeat = [],//未重复
     len = array.length,
-    num = 0;
-  if (object != null) {
-    var key = object.key,
-      flag = object.flag;
+    key = !!object ? object.key : null,
+    flag = !!object ? object.flag : null;
+  type = type!=null ? type : 0;
+  if (!Array.isArray(array)) {
+    return [];
   }
-  if (Array.isArray(array)) {
-    if (object == null || key == null) {
-      array.concat().sort().sort(function (a, b) {
-        num++;
-        if (a == b && arr.indexOf(a) === -1) {
-          arr.push(a);
+  if (array.length<=1) {
+    return array;
+  }
+  if (key == null) {
+    // 排序后数组才能左右做比较
+    let tempArr = array.concat().sort();
+    tempArr.forEach(function(ele,index,arr){
+      let a = arr[index],
+        b = arr[index>=len-1 ? index : index+1];
+      if (a == b && repeat.indexOf(a) === -1) {
+        repeat.push(a);
+      }
+      if (repeat.indexOf(a) === -1) {
+        noRepeat.push(a);
+      }
+      if (index == len-1) {
+        if (repeat.indexOf(b) === -1) {
+          noRepeat.push(b);
         }
-        if (arr.indexOf(a) === -1) {
-          arr1.push(a);
+      }
+    })
+  } else {
+    let tempArr = array.concat().sort(function (a, b) {
+      if (typeof a[key] == "number") {
+        return a[key] - b[key]
+      } else {
+        return a[key].localeCompare(b[key])
+      }
+    });
+    tempArr.forEach(function (ele, index, arr) {
+      let a = arr[index],
+        b = arr[index>=len-1 ? index : index+1];
+      if(!flag){
+        if (a[key] == b[key] && repeat.indexOf(a[key]) === -1) {
+          repeat.push(a[key]);
         }
-        if (num==len-1) {
-          if (arr.indexOf(b) === -1) {
-            arr1.push(b);
+        if (repeat.indexOf(a[key]) === -1) {
+          noRepeat.push(a[key]);
+        }
+        if (index==len-1) {
+          if (repeat.indexOf(b[key]) === -1) {
+            noRepeat.push(b[key]);
           }
         }
-      });
-    } else {
-      var tArr = [];
-      array.concat().sort(function (a, b) {
-        if (typeof a[key] == "number") {
-          return a[key] - b[key]
-        } else {
-          return a[key].localeCompare(b[key])
+      } else {
+        let keyArr = [];//key值数组
+        if (a[key] == b[key] && repeat.indexOf(a[key]) === -1) {
+          repeat.push(a);
+          repeat.push(b);
+          keyArr = repeat.map(function (ele){
+            return ele[key]
+          })
         }
-      }).sort(function (a, b) {
-        num++;
-        if (a[key] == b[key] && arr.indexOf(a[key]) === -1) {
-          if (!flag) {
-            arr.push(a[key]);
-          } else {
-            arr.push(a);
-            arr.push(b);
-            tArr = arr.map(function (ele){
-              return ele[key]
-            })
+        if (keyArr.indexOf(a[key]) === -1) {
+          noRepeat.push(a);
+        }
+        if (index==len-1) {
+          if (keyArr.indexOf(b[key]) === -1) {
+            noRepeat.push(b);
           }
         }
-        if (!flag) {
-          if (arr.indexOf(a[key]) === -1) {
-            arr1.push(a[key]);
-          }
-          if (num==len-1) {
-            if (arr.indexOf(b[key]) === -1) {
-              arr1.push(b[key]);
-            }
-          }
-        } else {
-          if (tArr.indexOf(a[key]) === -1) {
-            arr1.push(a);
-          }
-          if (num==len-1) {
-            if (tArr.indexOf(b[key]) === -1) {
-              arr1.push(b);
-            }
-          }
-        }
-      });
-    }
+      }
+    });
   }
   switch (type) {
     case 0:
-      return arr;
+      return JSON.stringify(repeat.concat(noRepeat));
     case 1:
-      return arr1;
+      return repeat;
     case 2:
-      return arr.concat(arr1);
+      return noRepeat;
     default:
-      break;
+      return []
   }
 }
-// console.log(refrain([1,2,3,5,2,3],false))
-// console.log(refrain([1,2,3,5,2,3],true))
-// console.log(JSON.stringify(refrain([
+// console.log(operRepeatArray(["ab","ad","ab","ad","ab","ad","ac","ae","ac","ae","ac","ae"],0))
+// console.log(operRepeatArray([1,2,3,2,3,2,3,4,5,4,5,4,5],0))
+// console.log(operRepeatArray(["ab","ad","ab","ad","ab","ad","ac","ae","ac","ae","ac","ae",1,2,3,2,3,2,3,4,5,4,5,4,5],0))
+// console.log(JSON.stringify(operRepeatArray([
 //   {
 //     num: "bbbccb6d-2a03-4085-b71c-3ae54a0f7b7a",
 //     state: false
@@ -118,10 +122,28 @@ function refrain(array, type, object) {
 //     num: "9cde25d3-f9ce-49e2-845e-7ee0cd4dbe30",
 //     state: true
 //   }
-// ],true,{
+// ],0,{
 //   key: "num",
-//   flag: true
+//   flag: false
 // })))
+
+/**
+ * 高性能数组去重(不会破坏已有排序,未重复取第一个位置)
+ * @param {Array} array 源数组 
+ * 
+ * @returns {Array} 返回数组
+ */
+function distinct(array) {
+  let result = [],
+      obj = {};//利用对象属性不重复
+  for (let i of array) {
+      if (!obj[i]) {
+          result.push(i);
+          obj[i] = 1;
+      }
+  }
+  return result
+}
 
 /**
  * 去除数组指定元素
@@ -132,32 +154,24 @@ function refrain(array, type, object) {
  * @returns {Array} 返回数组
  */
 function removeItem(arr,removeArr,key) {
-  if (!!key) {
-    for (var i = 0; i < arr.length; i++) {
-      if (removeArr.indexOf(arr[i][key]) != -1) {
-        arr.splice(i, 1);
-        i--;
-      }
-    }
-  } else {    
-    for (var i = 0; i < arr.length; i++) {
-      if (removeArr.indexOf(arr[i]) != -1) {
-        arr.splice(i, 1);
-        i--;
-      }
+  for (var i = 0; i < arr.length; i++) {
+    if (key==null ? removeArr.indexOf(arr[i]) != -1 : removeArr.indexOf(arr[i][key]) != -1) {
+      arr.splice(i, 1);
+      i--;
     }
   }
   return arr
 }
-console.log(removeItem([3, 7, 11, 0, 0, 0, 3, 0, 55],[11,55]))
-console.log(removeItem([{
-  id: "1"
-},{
-  id: "2"
-},{
-  id: "3"
-}],["1","3"],"id"))
+// console.log(removeItem([3, 7, 11, 0, 0, 0, 3, 0, 55],[0,55]))
+// console.log(removeItem([{
+//   id: "1"
+// },{
+//   id: "2"
+// },{
+//   id: "3"
+// }],["1","3"],"id"))
 module.exports =  {
-  refrain,
+  operRepeatArray,
+  distinct,
   removeItem
 }
