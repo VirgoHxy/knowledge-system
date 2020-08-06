ccc64ee1423fbd1736abf191552ec239f56e9555
17764092501 <1061811549@qq.com>
2020-08-06 17:58:28 +0800

    数组方法修改

--- arr.js
+++ arr.js
@@ -1,3 +1,59 @@
+let arr = [1, 2, 3];
+
+// 对象方法
+newArr = Array.from("hxy",ele => {return ele + 1} , this); // ["h1","x1","y1"] 对拥有length属性的对象或可迭代的对象来返回一个数组 返回新数组
+flag = Array.isArray(arr) // true 判断一个对象是否为数组
+console.log(flag)
+
+// 实例方法
+// es5
+newArr = arr.concat([1], [2]) // [1,2,3,1,2] 连接多个数组 返回新数组 不改变原数组
+flag = arr.every(ele => { // false 检测数组中的所有元素是否都符合条件
+  return ele > 1
+})
+flag = arr.some(ele => { // true 检测数组中是否有一个或多个元素符合条件
+  return ele > 1
+})
+newArr = arr.filter(ele => { // [2,3] 返回一个符合条件的数组 重复元素只返回一次 返回新数组 不改变原数组
+  return ele > 1
+})
+arr.forEach((ele, index, arr) => {}, this) // 遍历数组 ele 数组元素 index 索引 arr 原数组 this代表回调函数的this指向
+newArr = arr.map((ele, index, arr) => {return ele+1}, this) // [2,3,4] 遍历数组 返回新数组 ele 数组元素 index 索引 arr 原数组 this代表回调函数的this指向
+index = arr.indexOf(1,2) // -1 从2索引ltr开始判断一个数组是否包含1 包含返回索引 不包含返回-1
+index = arr.lastIndexOf(1,2) // 0 从2索引rtl开始判断一个数组是否包含1 包含返回索引 不包含返回-1
+str = arr.join("-") // 1-2-3 把数组中的所有元素转换一个字符串
+arr.push(4) // 4 向数组的末尾添加一个或更多元素 返回数组长度 改变原数组
+arr.pop() // 4 删除数组最后一个元素 返回删除元素 改变原数组
+arr.unshift(0) // 4 向数组的头部添加一个或更多元素 返回数组长度 改变原数组
+arr.shift() // 0 删除数组第一个元素 返回删除元素 改变原数组
+newArr = arr.slice(1,2) // [2] 从1索引返回(2-1)个选定的元素 返回新数组 不改变原数组
+arr.reverse() // [3,2,1] 颠倒数组中元素顺序 改变原数组
+arr.sort((a,b) => { return a - b }); // [1,2,3] 数组的元素进行排序 改变原数组
+console.log(newArr)
+
+// es6
+arr.copyWithin(1, 0, 2) // [1,1,2] 从数组的第0位后2位获取元素(1,2)复制到第1位后 改变原数组
+newArr = arr.entries() // 返回一个新的Array Iterator对象 该对象包含数组中每个索引的键/值对 返回新数组 不改变原数组
+newArr.next() // { value: [ 0, 1 ], done: false }
+newArr.next() // { value: [ 1, 1 ], done: false }
+newArr.next() // { value: [ 2, 2 ], done: false }
+newArr.next() // { value: undefined, done: true }
+newArr = arr.keys() // 返回一个新的Array Iterator对象 该对象包含数组中每个索引的键 返回新数组 不改变原数组
+newArr.next() // { value: 0, done: false }
+newArr.next() // { value: 1, done: false }
+newArr.next() // { value: 2, done: false }
+newArr.next() // { value: undefined, done: true }
+new Array(5).fill(0) // [0,0,0,0,0] 将数组元素都填充0 改变原数组
+ele = arr.find(ele => { // 2 返回第一个符合条件的元素 返回元素 不改变原数组
+  return ele > 1
+})
+index = arr.findIndex(ele => { // 0 返回第一个符合条件的元素的索引 返回索引 不改变原数组
+  return ele > 0
+})
+flag = arr.includes(1,2) // false 从2索引开始判断一个数组是否包含1 包含返回true 不包含返回false
+
+console.log(flag)
+
 /**
  * 操作重复数组,获取数组重复/未重复/去重复的元素(会破坏已有排序)
  * @param {Array} array 源数组 
@@ -165,9 +221,9 @@ function distinct1() {
  */
 function removeItem(arr, removeArr, key) {
   if (!key) {
-    return arr.filter(item=>removeArr.indexOf(item)==-1)
+    return arr.filter(item => removeArr.indexOf(item) == -1)
   }
-  return arr.filter(item=>removeArr.indexOf(item[key])==-1)
+  return arr.filter(item => removeArr.indexOf(item[key]) == -1)
 }
 // console.log(removeItem([3, 7, 11, 0, 0, 0, 3, 0, 55],[0,55]))
 // console.log(JSON.stringify(removeItem([{
